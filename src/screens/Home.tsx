6dc7801be69cafa6e50b5ceb162837ff99e79d8e
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-19 09:52:12 -0300

    Geramos os dias e componentes HabitDay e Cabeçalho

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearsStart = generateRangeDatesFromYearStart();
+const minSummaryDateSizes = 18 * 7;
+const amountOfDaysToFill = minSummaryDateSizes - datesFromYearsStart.length;
 
 export function Home() {
     return (
@@ -20,7 +24,34 @@ export function Home() {
                     ))
                 }
             </View>
-            <HabitDay />
+
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            >
+                <View
+                    className="flex-row flex-wrap"
+                >
+                    {
+                        datesFromYearsStart.map(date => (
+                            <HabitDay
+                                key={date.toISOString()}
+                            />
+                        ))
+                    }
+                    {
+                        amountOfDaysToFill > 0 && Array
+                            .from({ length: amountOfDaysToFill })
+                            .map((_, index) => (
+                                <View
+                                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                                />
+                            ))
+                    }
+                </View>
+            </ScrollView>
+
         </View>
     )
 }
