65c60fe39d17d6d322855fb468482a5555560eca
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-19 08:27:33 -0300

    Cirando a Home: Logo, Tipagem de componentes e Icones

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header";
 
 export function Home() {
     return (
-        <View className="flex bg-background px-8 pt-16" >
+        <View className="flex-1 bg-background px-8 pt-16" >
             <Header />
         </View>
     )
