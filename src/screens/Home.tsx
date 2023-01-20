d4d3957db53570ae8a03f047bf3fb7e94f0d782a
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-20 16:51:23 -0300

    Configuração de acesso ao servidor

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
+import { api } from '../lib/axios';
+import { useEffect, useState } from "react";
+
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearsStart = generateRangeDatesFromYearStart();
@@ -10,7 +13,30 @@ const minSummaryDateSizes = 18 * 7;
 const amountOfDaysToFill = minSummaryDateSizes - datesFromYearsStart.length;
 
 export function Home() {
-    const { navigate } = useNavigation()
+    const [loading, setLoading] = useState(true);
+    const [summary, setSummary] = useState(null);
+    const { navigate } = useNavigation();
+
+    async function fetchData() {
+        try {
+            console.log('Entrou!');
+            setLoading(true);
+            const response = await api.get('/summary');
+            console.log(`Dados: ${response.data}`);
+            setSummary(response.data);
+            console.log('Saiu!')
+        } catch (error) {
+            Alert.alert('Ops', 'Não foi possivel carregar o sumário de hábtios.')
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => { 
+        fetchData();
+     }, []);
+
     return (
         <View className="flex-1 bg-background px-8 pt-16" >
             <Header />
