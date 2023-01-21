e0ed64f71696ac697ca00629e16bb2ab473764b5
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-01-21 10:54:21 -0300

    não poder editar datas passadas

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -9,6 +9,7 @@ import { Loading } from "../components/Loading";
 import { HabitEmpty } from "../components/HabitEmpty";
 import { api } from "../lib/axios";
 import dayjs from "dayjs";
+import clsx from "clsx";
 
 
 interface Params {
@@ -30,6 +31,7 @@ export function Habit() {
     const route = useRoute();
     const { date } = route.params as Params;
     const parsedDate = dayjs(date);
+    const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
     const dayOfWeek = parsedDate.format('dddd');
     const dayAndMonth = parsedDate.format('DD/MM');
     const habitsProgress = dayInfo?.possibleHabits.length 
@@ -78,8 +80,12 @@ export function Habit() {
                 <Text className="text-white font-extrabold text-3xl">
                     {dayAndMonth}
                 </Text>
+
                 <ProgressBar progress={habitsProgress} />
-                <View className="mt-6">
+
+                <View className={clsx("mt-6", {
+                    ['opacity-50']: isDateInPast
+                })}>
                     {dayInfo?.possibleHabits ?
                         dayInfo?.possibleHabits.map(habit =>
 
@@ -87,12 +93,21 @@ export function Habit() {
                                 key={habit.id}
                                 title={habit.title}
                                 checked={completedHabits.includes(habit.id)}
+                                disabled={isDateInPast}
                                 onPress={() => handleToggleHabit(habit.id)}
                             />
                         )
                         : <HabitEmpty />
                     }
                 </View>
+                
+                {
+                    isDateInPast && (
+                    <Text className="text-white mt-10 text-center">
+                        Você não pode editar hábitos de uma data passada.
+                    </Text>
+                    )
+                }
             </ScrollView>
         </View>
     )
